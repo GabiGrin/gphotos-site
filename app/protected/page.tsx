968cf092d605d341e3315c537c33c9fac685fdca
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-10-25 15:06:02 +0300

    bob

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,37 +1,143 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { createClient } from "@/utils/supabase/server";
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
 import { InfoIcon } from "lucide-react";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function ProtectedPage() {
-  const supabase = await createClient();
+export default function ProtectedPage() {
+  const supabase = createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const [user, setUser] = useState<User | null>(null);
+
+  const [pickerUrl, setPickerUrl] = useState<string | null>(null);
+  const [sessionId, setSessionId] = useState<string | null>(null);
+
+  const [imagesSet, setImagesSet] = useState(false);
+  const [googleAccessToken, setGoogleAccessToken] = useState<string | null>(
+    null
+  );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await supabase.auth.getUser();
+      setUser(user.data.user);
+      if (!user.data.user) {
+        router.push("/sign-in");
+      } else {
+        getGoogleAccessToken();
+      }
+    };
+
+    getUser();
+  }, []);
+
+  async function getGoogleAccessToken() {
+    const session = await supabase.auth.getSession();
+
+    if (!session || !session.data.session) {
+      console.error("User is not logged in");
+      return;
+    }
+
+    console.log("Session:", session);
+
+    const googleAccessToken = session.data.session.provider_token;
+    setGoogleAccessToken(googleAccessToken!);
+
+    const response = await fetch(
+      "https://photospicker.googleapis.com/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + googleAccessToken,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log("Session created:", responseData);
+        setSessionId(responseData.id);
+        setPickerUrl(responseData.pickerUri);
+        return responseData;
+      });
+
+    console.log("Google Access Token:", googleAccessToken);
+    return googleAccessToken;
+  }
+
+  useEffect(() => {
+    if (pickerUrl && !imagesSet) {
+      const timer = setInterval(async () => {
+        console.log("Checking if images are set");
+
+        const session_url = "https://photospicker.googleapis.com/v1/sessions/";
+
+        const sessionResponse = await fetch(session_url + sessionId, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + googleAccessToken,
+          },
+        }).then((response) => response.json());
+
+        if (sessionResponse.mediaItemsSet) {
+          setImagesSet(true);
+        }
+
+        console.log("Session response:", sessionResponse);
+      }, 5000);
+      return () => clearInterval(timer);
+    }
+  }, [pickerUrl, imagesSet]);
+
+  useEffect(() => {
+    if (imagesSet) {
+      let itemsQuery = `sessionId=${sessionId}&pageSize=100`;
+
+      fetch(`https://photospicker.googleapis.com/v1/mediaItems?${itemsQuery}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + googleAccessToken,
+        },
+      })
+        .then((response) => response.json())
+        .then((responseData) => {
+          console.log("Response:", responseData);
+        });
+    }
+  }, [imagesSet]);
 
   if (!user) {
-    return redirect("/sign-in");
+    return "Loading..";
   }
+
+  // const googleAccessToken = await getGoogleAccessToken();
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
+      <div className="flex flex-col items-center gap-4">
+        {pickerUrl && (
+          <a
+            href={pickerUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            Open Google Photos Picker
+          </a>
+        )}
+
+        {!imagesSet && pickerUrl && (
+          <p className="text-gray-600">Waiting for user selection...</p>
+        )}
+
+        {imagesSet && <p className="text-green-600">Images are uploading...</p>}
       </div>
     </div>
   );
