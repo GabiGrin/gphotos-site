31a40970ef7a74700aaa09a8c06a8b2a4f213962
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-10-26 17:25:32 +0300

    better thumbnail

--- utils/create-thumbnail.ts
+++ utils/create-thumbnail.ts
@@ -2,14 +2,15 @@ import sharp from "sharp";
 
 export async function createThumbnail(
   imageBlob: Blob,
-  width: number,
-  height: number
+  maxWidth: number,
+  maxHeight: number,
+  format: "jpeg" | "png" | "webp" = "webp"
 ): Promise<Blob> {
   const buffer = await imageBlob.arrayBuffer();
   const resizedBuffer = await sharp(buffer)
-    .resize(width, height, { fit: "cover" })
-    .toFormat("jpeg")
+    .resize(maxWidth, maxHeight, { fit: "inside", withoutEnlargement: true })
+    .toFormat(format)
     .toBuffer();
 
-  return new Blob([resizedBuffer], { type: "image/jpeg" });
+  return new Blob([resizedBuffer], { type: `image/${format}` });
 }
