e8a87bfbce426b4ac07fd49c0ef62387617e50b8
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-11-12 11:18:02 +0200

    fix build

--- app/app/edit/page.tsx
+++ app/app/edit/page.tsx
@@ -214,6 +214,7 @@ export default function DashboardPage() {
           images={processedImages}
           albums={albums}
           showBranding={limits.branding}
+          hostname={site.username}
         />
       )}
     </div>
