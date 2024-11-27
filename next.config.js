8e4db33cc7c51b7b05f7ff2ba88c1e58ef4c8f5c
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-11-27 17:10:13 +0200

    fix rewrites

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+// next.config.js
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/zipzap/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/zipzap/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/zipzap/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 module.exports = nextConfig;
