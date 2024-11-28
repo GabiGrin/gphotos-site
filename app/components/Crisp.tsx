592e3ea4de48c27b0380900658ac87180315d1fc
Gabriel Grinberg <gabi.grinberg@gmail.com>
2024-11-28 10:44:53 +0200

    fix cripst

--- app/components/Crisp.tsx
+++ app/components/Crisp.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("MY_CRISP_WEBSITE_ID");
+    Crisp.configure("d7bd9715-2e8b-4739-b9a9-6dbbb61e1ca5");
   });
 
   return null;
